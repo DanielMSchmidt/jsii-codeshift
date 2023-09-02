ca01a37d6f00fe459ffe1c8521aec7255bfd3332
Daniel Schmidt <danielmschmidt92@gmail.com>
2023-09-02 12:10:07 +0200

    add data structure for imports

--- src/ast/import.rs
+++ src/ast/import.rs
@@ -1,1 +1,35 @@
-pub struct ImportDeclaration {}
+use super::ast::AstNode;
+use std::fmt::{self, Debug, Display};
+
+#[derive(Debug)]
+pub enum ImportSpecifier {
+    Namespace(String),
+    Item { imported: String, local: String },
+}
+
+impl Display for ImportSpecifier {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+#[derive(Debug)]
+pub struct ImportDeclaration {
+    pub source: String,
+    // importKind (value vs type) omitted for now
+    pub specifiers: Vec<ImportSpecifier>,
+}
+
+impl Display for ImportDeclaration {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "import(source: {}, specifiers: {})",
+            self.source,
+            self.specifiers
+                .iter()
+                .fold(String::new(), |a, spec| format!("{}, {}", a, spec))
+        )
+    }
+}
+impl AstNode for ImportDeclaration {}
